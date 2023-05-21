1d171b5e8fd8cf4a6ca12d9c46f9ce6012f10805
demelcjohn <demal.john007@gmail.com>
2023-05-21 21:13:57 +0530

    random integer generation commit

--- main.cpp
+++ main.cpp
@@ -1,17 +1,34 @@
 #include <iostream>
 #include <random>
+#define SIZE 1000
 
 int main()
 {
     std::random_device rd;
     std::mt19937 generator(rd());
-
-    int lowerBound = 1;
-    int upperBound = 100;
-
+    int lowerBound = -10, upperBound = 10;
     std::uniform_int_distribution<int> distribution(lowerBound, upperBound);
 
-    randomInt = distribution(generator);
+    int a[SIZE][SIZE], b[SIZE][SIZE], n, randomInt;
 
-    return 0;
+    n = 3;
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            randomInt = distribution(generator);
+            a[i][j] = randomInt;
+        }
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            randomInt = distribution(generator);
+            b[i][j] = randomInt;
+        }
+    }
+
+        return 0;
 }
