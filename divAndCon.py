da2bc44ed956e2a4d4fd7d61ef531c5920a1f258
demelcjohn <demal.john007@gmail.com>
2023-05-22 22:15:57 +0530

    addMatrices added

--- divAndCon.py
+++ divAndCon.py
@@ -1,13 +1,28 @@
 import numpy as np
 
 
-def divAndCon(matrix_a, matrix_b):
-    r1, c1 = matrix_a.shape
-    r2, c2 = matrix_b.shape
+def divAndCon(matrixA, matrixB):
+    r1, c1 = matrixA.shape
+    r2, c2 = matrixB.shape
     if (r1 == 1 and r2 == 1 and c1 == 1 and c2 == 1):
         result = np.empty((1, 1), dtype=int)
-        result[0] = matrix_a[0]*matrix_b[0]
+        result[0] = matrixA[0]*matrixB[0]
         return result
+    A11, A12, A21, A22 = splitMatrix(matrixA)
+    B11, B12, B21, B22 = splitMatrix(matrixB)
+
+    P1 = divAndCon(A11, B11)
+    P2 = divAndCon(A12, B21)
+    P3 = divAndCon(A11, B12)
+    P4 = divAndCon(A12, B22)
+    P5 = divAndCon(A21, B11)
+    P6 = divAndCon(A22, B21)
+    P7 = divAndCon(A21, B12)
+
+    C11 = addMatrices(P1, P2)
+    C12 = addMatrices(P3, P4)
+    C13 = addMatrices(P5, P6)
+    C14 = addMatrices(addMatrices(P7, P1), addMatrices(-P3, P5))
 
 
 def splitMatrix(matrix):
@@ -32,3 +47,12 @@ def splitMatrix(matrix):
             elif (i >= r1 and j >= c1):
                 D[i-r1][j-c1] = matrix[i][j]
     return A, B, C, D
+
+
+def addMatrices(A, B):
+    r, c = A.shape
+    C = np.empty((r, c), dtype=int)
+    for i in range(r):
+        for j in range(c):
+            C[i][j] = A[i][j]+B[i][j]
+    return C
