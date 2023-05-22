369719f884ed65dfc7e24fede777d2ee86d8ad00
demelcjohn <demal.john007@gmail.com>
2023-05-22 21:22:47 +0530

    list to array

--- matrix.py
+++ matrix.py
@@ -2,32 +2,36 @@ import random
 from iterative import iterative
 from display import display
 import timeit
+import numpy as np
+
 
 def generateElements(matrix):
     lower_bound = -10
+
     upper_bound = 10
-    n =3
-    for i in range(0,n):
-        row = []
-        for j in range(0,n):
+    n = 3
+    for i in range(0, n):
+        for j in range(0, n):
             random_int = random.randint(lower_bound, upper_bound)
-            row.append(random_int)
-        matrix.append(row)
+            matrix[i, j] = random_int
+
     return matrix
 
 
-matrixA = []
+n = 3
+
+matrixA = np.empty((n, n), dtype=int)
 matrixA = generateElements(matrixA)
 
-matrixB = []
+matrixB = np.empty((n, n), dtype=int)
 matrixB = generateElements(matrixB)
 
 
-# display(matrixA)
-# display(matrixB)
+display(matrixA)
+display(matrixB)
 
 start_time = timeit.default_timer()
-result = iterative(matrixA,matrixB)
+result = iterative(matrixA, matrixB)
 end_time = timeit.default_timer()
 runtime = end_time - start_time
 print("Runtime:", runtime, "seconds")
