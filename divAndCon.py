132c6e40deebd908f9eaf836084941db244d6a0d
demelcjohn <demal.john007@gmail.com>
2023-05-23 08:58:15 +0530

    changed algorithm, first working execution

--- divAndCon.py
+++ divAndCon.py
@@ -1,12 +1,23 @@
 import numpy as np
+from display import display
 
 
 def divAndCon(matrixA, matrixB):
     r1, c1 = matrixA.shape
     r2, c2 = matrixB.shape
-    if (r1 == 1 and r2 == 1 and c1 == 1 and c2 == 1):
-        result = np.empty((1, 1), dtype=int)
-        result[0] = matrixA[0]*matrixB[0]
+
+    if (r1 == 1 or r2 == 1 or c2 == 1):
+        result = np.empty((r1, c2), dtype=int)
+        for i in range(r1):
+            for j in range(c2):
+                result[i][j] = 0
+                for k in range(r2):
+                    result[i][j] += matrixA[i][k] * matrixB[k][j]
+        # print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&Dim", r1, c1, r2, c2)
+        # display(matrixA)
+        # display(matrixB)
+        # display(result)
+        # print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
         return result
     A11, A12, A21, A22 = splitMatrix(matrixA)
     B11, B12, B21, B22 = splitMatrix(matrixB)
@@ -18,11 +29,13 @@ def divAndCon(matrixA, matrixB):
     P5 = divAndCon(A21, B11)
     P6 = divAndCon(A22, B21)
     P7 = divAndCon(A21, B12)
+    P8 = divAndCon(A22, B22)
 
     C11 = addMatrices(P1, P2)
     C12 = addMatrices(P3, P4)
     C21 = addMatrices(P5, P6)
-    C22 = addMatrices(addMatrices(P7, P1), addMatrices(-P3, P5))
+    C22 = addMatrices(P7, P8)
+    # addMatrices(P7, P1), addMatrices(-P3, P5))
 
     Result = combineMatrices(C11, C12, C21, C22)
 
