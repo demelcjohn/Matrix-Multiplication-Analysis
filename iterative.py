0daaed5afc4f6a265bd9292d0e3ee1a489f10525
demelcjohn <demal.john007@gmail.com>
2023-05-23 08:58:36 +0530

    changed to array from list

--- iterative.py
+++ iterative.py
@@ -1,10 +1,14 @@
-def iterative(matrix_a, matrix_b):
-    n = len(matrix_a)
+import numpy as np
 
-    result = [[0] * n for _ in range(n)]
+
+def iterative(matrix_a, matrix_b):
+    n, c = matrix_a.shape
+
+    result = np.empty((n, n), dtype=int)
 
     for i in range(n):
         for j in range(n):
+            result[i][j] = 0
             for k in range(n):
                 result[i][j] += matrix_a[i][k] * matrix_b[k][j]
 
