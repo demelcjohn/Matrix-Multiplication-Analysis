2e1b2579c972a431962fd251ad0a8873484a608e
demelcjohn <demal.john007@gmail.com>
2023-06-07 07:46:15 +0530

    multiple test case

--- matrix.py
+++ matrix.py
@@ -5,6 +5,7 @@ import timeit
 import numpy as np
 from divAndCon import divAndCon, combineMatrices
 from strassen import strassen
+import csv
 
 
 def generateElements(matrix, n):
@@ -18,44 +19,69 @@ def generateElements(matrix, n):
     return matrix
 
 
-n = 3
+data = [
+    ['n', 'iterativeTime', 'divConTime', 'strassenTime']
+]
 
-matrixA = np.empty((n, n), dtype=int)
-matrixA = generateElements(matrixA, n)
+i = 0
+while i < 100:
+    # if ~(i&(i-1))!=0:
+    i = i+2
+    n = i
 
-matrixB = np.empty((n, n), dtype=int)
-matrixB = generateElements(matrixB, n)
+    for repeat in range(2):
 
+        row = []
+        # n = 4
+        row.append(n)
 
-print("matrix A")
-display(matrixA)
-print("matrix B")
-display(matrixB)
+        matrixA = np.empty((n, n), dtype=int)
+        matrixA = generateElements(matrixA, n)
 
-start_time = timeit.default_timer()
-result = iterative(matrixA, matrixB)
-end_time = timeit.default_timer()
-runtime = end_time - start_time
-print("Runtime:", runtime, "seconds")
-print("Iterative result")
+        matrixB = np.empty((n, n), dtype=int)
+        matrixB = generateElements(matrixB, n)
 
-display(result)
+        # print("matrix A")
+        # display(matrixA)
+        # print("matrix B")
+        # display(matrixB)
 
+        start_time = timeit.default_timer()
+        result = iterative(matrixA, matrixB)
+        end_time = timeit.default_timer()
+        runtime = end_time - start_time
+        # print("Runtime:", runtime, "seconds")
+        # print("Iterative result")
 
-start_time = timeit.default_timer()
-result = divAndCon(matrixA, matrixB)
-end_time = timeit.default_timer()
-runtime = end_time - start_time
-print("Runtime:", runtime, "seconds")
-print("Divide and conquer result")
+        # display(result)
+        row.append(runtime)
 
-display(result)
+        start_time = timeit.default_timer()
+        result = divAndCon(matrixA, matrixB)
+        end_time = timeit.default_timer()
+        runtime = end_time - start_time
+        # print("Runtime:", runtime, "seconds")
+        # print("Divide and conquer result")
 
-start_time = timeit.default_timer()
-result = strassen(matrixA, matrixB)
-end_time = timeit.default_timer()
-runtime = end_time - start_time
-print("Runtime:", runtime, "seconds")
-print("Strassen result")
+        # display(result)
+        row.append(runtime)
 
-display(result)
+        start_time = timeit.default_timer()
+        result = strassen(matrixA, matrixB)
+        end_time = timeit.default_timer()
+        runtime = end_time - start_time
+        # print("Runtime:", runtime, "seconds")
+        # print("Strassen result")
+
+        # display(result)
+        row.append(runtime)
+
+        data.append(row)
+
+        csv_file = 'data.csv'
+
+        with open(csv_file, 'w', newline='') as file:
+            writer = csv.writer(file)
+
+            for row in data:
+                writer.writerow(row)
